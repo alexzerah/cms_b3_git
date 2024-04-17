b302e9e5704275f18b9b93050e8cdbea573ae7ac
Alex Zerah <alex.zerah@gmail.com>
2024-04-17 12:10:31 +0200

    delete

--- main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('h1').addEventListener('mouseover', () => {
-  document.querySelector('body').style.backgroundColor = 'green';
-});
-
-document.querySelector('h1').addEventListener('mouseleave', () => {
-  document.querySelector('body').style.backgroundColor = 'white';
-});
